26392f299a6335add306a6c69fb209230056d29c
kaitsh@d-git.de <daniel.glinka@neuland.com.py>
2019-02-11 10:06:16 -0300

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,42 @@
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
+import { states, actions } from '../store.js'
+
+import Header from './header';
+
+// Code-splitting is automated for routes
+import Home from '../routes/home';
+import Profile from '../routes/profile';
+
+export default class App extends Component {
+	
+	/** Gets fired when the route changes.
+	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+	 *	@param {string} event.url	The newly routed URL
+	 */
+	handleRoute = e => {
+		this.currentUrl = e.url;
+	};
+
+  componentWillMount() {
+    var setState = this.setState.bind(this);
+    states.map(state => {
+      setState(state);
+    });
+  }
+
+	render() {
+    var state = this.state;
+		return (
+			<div id="app">
+				<Header />
+				<Router onChange={this.handleRoute}>
+					<Home path="/" />
+				  <Profile state={state.profile1} actions={actions.profile1} path="/profile/" user="me" />
+				  <Profile state={state.profile2} actions={actions.profile2} path="/profile/:user" />
+				</Router>
+        <pre>{JSON.stringify(state, null, 4)}</pre>
+			</div>
+		);
+	}
+}
---
+++ src/store.js
@@ -0,0 +1,52 @@
+import flyd from 'flyd';
+
+const profile1 = {
+  initialState: {
+    count: 10,
+    blah: 12
+  },
+  actions: update => {
+    return {
+      increment: () => {
+        update(state => {
+          setTimeout( () => state.profile1.count += 1, 3000);
+          return state;
+        });
+      }
+    };
+  }
+};
+const profile2 = {
+  initialState: {
+    count: 10,
+  },
+  actions: update => {
+    return {
+      increment: () => {
+        update(state => {
+          state.profile2.count += 1;
+          return state;
+        });
+      }
+    };
+  }
+};
+
+const app = {
+  initialState: {
+    profile1: profile1.initialState,
+    profile2: profile2.initialState
+  },
+  actions: update => {
+    return {
+      profile1: profile1.actions(update),
+      profile2: profile2.actions(update)
+    };
+  }
+};
+
+const update = flyd.stream();
+
+export const states = flyd.scan((state, patch) => patch(state), app.initialState, update);
+
+export const actions = app.actions(update);
